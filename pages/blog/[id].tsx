8266a71a89ed57ac7ca8c128faf583cff6c9917d
Silo Chad <silochad@protonmail.com>
2022-03-16 15:08:46 -0500

    add prose customization

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -41,13 +41,13 @@ export default function Post({ title, content, date, image } : PostProps) {
         image={image}
       />
       <ContentWrapper variant="default">
-        <div className="space-y-12">
+        <div className="space-y-8">
           <div className="space-y-2 border-b border-gray-100 pb-8">
             <p className="text-sm text-slate-400">{date}</p>
             <h2 className="text-4xl font-bold text-slate-900">{title}</h2>
           </div>
           <div
-            className="font-normal text-lg prose"
+            className={`text-md prose`}
             dangerouslySetInnerHTML={{ __html: content }}
           />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        // Default styling for Typography plugin.
+        DEFAULT: {
+          css: {
+            // maxWidth: 'none',
+            h1: {
+              // fontSize: '2rem',
+              // marginTop: '2.5rem',
+              // marginBottom: '0.5rem',
+            },
+            a: {
+              color: theme('colors.blue.500'),
+            }
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
