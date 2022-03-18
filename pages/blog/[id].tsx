2c8893efbf8f43234a332e0d48213e9c93ad9105
Cool Bean <anoncoolbean@gmail.com>
2022-03-18 15:51:30 -0700

    Add logs

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -75,10 +75,9 @@ const Post: NextPage<PostProps> = (props) => {
     description
   } = props;
 
-  var siteUrl = process.env.SITE_URL || "https://bean.money";
-  if (typeof window !== 'undefined') {
-    siteUrl = window.location.origin;
-  }
+  const siteUrl = (typeof window !== 'undefined') ? window.location.origin : "https://bean.money";
+  console.log("SiteURL: " + siteUrl);
+
 
   return (
     <>
