7d2fa6dfd2222162d8f69b04839233ca940f884b
Cool Bean <anoncoolbean@gmail.com>
2022-03-17 22:20:16 -0700

    Hardcode post ids

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -48,7 +48,11 @@ export async function getStaticProps({ params } : PostData) : Promise<GetStaticP
 }
 
 export async function getStaticPaths() : Promise<GetStaticPathsResult> {
-  const paths = getAllPostIds()
+  // const paths = getAllPostIds()
+  const paths = [
+    { params: { id: 'bip-12-silo-generalization-i' } },
+    { params: { id: 'earn-interest-from-beanstalk-just-the-basics' } }
+  ]
   return {
     paths,
     fallback: true
