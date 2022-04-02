f46b1b1ec8d6d7527adc5f1250c8402252725152
anoncoolbean <99687271+anoncoolbean@users.noreply.github.com>
2022-04-02 14:47:24 -0700

    Remove extra CoinGecko footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,9 +13,6 @@ const Footer = () => (
       <a href="https://dune.xyz/tbiq/Beanstalk" target="_blank" rel="noreferrer">
         Dune
       </a>
-      <a href="https://www.coingecko.com/en/coins/bean" target="_blank" rel="noreferrer">
-        CoinGecko
-      </a>
       <a href="https://github.com/beanstalkfarms" target="_blank" rel="noreferrer" className="md:inline-block hidden">
         Github
       </a>
