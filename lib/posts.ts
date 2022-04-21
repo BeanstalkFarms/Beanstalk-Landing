83d1741f7e25d07bbbab23ec29959a67bf873c87
Silo Chad <silochad@protonmail.com>
2022-04-21 09:27:04 -0500

    cleanup

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,9 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import moment from 'moment'
-import { remark } from 'remark'
-import html from 'remark-html'
-import {unified} from "unified";
+import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeRaw from "rehype-raw";
