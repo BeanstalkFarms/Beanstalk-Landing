593d77e0e08aa03e0c5cd53ba4f47078f9ba9321
guy <28496268+hellofromguy@users.noreply.github.com>
2023-10-16 20:11:58 -0500

    update wp v2.6.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -128,7 +128,7 @@ const Home: NextPage<BlogProps> = ({ allPostsData, activeProposals }) => {
               rel="noreferrer"
               href="https://bean.money/beanstalk.pdf"
               icon="/assets/icon/beanstalk.svg"
-              desc="v2.5.0">
+              desc="v2.6.0">
               Whitepaper
             </Button>
             <Button
