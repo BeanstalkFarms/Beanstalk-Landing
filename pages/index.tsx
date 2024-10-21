ca9d9c8574d5c01668453c702b5f94a03b7f7106
beanstalkfarmsoperations <152643806+BeanstalkFarmsOperations@users.noreply.github.com>
2024-10-21 12:36:46 -0400

    update disclosures link

--- pages/index.tsx
+++ pages/index.tsx
@@ -128,7 +128,7 @@ const Home: NextPage<BlogProps> = ({ allPostsData, activeProposals }) => {
               rel="noreferrer"
               href="https://bean.money/beanstalk.pdf"
               icon="/assets/icon/beanstalk.svg"
-              desc="v2.7.0 (v3.0.0 coming soon)">
+              desc="v2.7.0 (v3.0.0 in progress)">
               Whitepaper
             </Button>
             <Button
